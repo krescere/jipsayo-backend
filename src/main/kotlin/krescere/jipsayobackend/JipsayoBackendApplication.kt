88289cc52b02ba66a0c3b7448b01225b449f4489
shininghyunho <gusgh3315@uos.ac.kr>
2022-12-17 20:49:33 +0900

    feat : init

---
+++ src/main/kotlin/krescere/jipsayobackend/JipsayoBackendApplication.kt
@@ -0,0 +1,11 @@
+package krescere.jipsayobackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JipsayoBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<JipsayoBackendApplication>(*args)
+}
