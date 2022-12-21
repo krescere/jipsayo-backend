4b30c849a2cd3a4c40fd999a297a33e6c77c8d7b
shininghyunho <gusgh3315@uos.ac.kr>
2022-12-22 03:30:41 +0900

    chore : rds 설정 경로를 수정했습니다.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/deploy
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -fl jipsayo | grep jar | awk '{print $1}')
@@ -10,26 +13,21 @@ if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -15 $CURRENT_PID"
-    # shellcheck disable=SC2086
     kill -15 $CURRENT_PID
     sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
-# shellcheck disable=SC2010
 JAR_NAME=$(ls $REPOSITORY/ |grep 'jipsayo' | tail -n 1)
 echo "> JAR Name: $JAR_NAME"
 
 # deploy dir 이동
-# shellcheck disable=SC2164
 cd $REPOSITORY
 
 echo "> $JAR_NAME 에 실행권한 추가"
-# shellcheck disable=SC2086
 sudo chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
 # 환경변수를 추가가 아닌 대체하는것!
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-rds.yml \
