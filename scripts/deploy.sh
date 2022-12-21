f950d5e3b662b69c716569f88d264bdb1ef38f75
shininghyunho <gusgh3315@uos.ac.kr>
2022-12-22 04:09:16 +0900

    chore : 개행 제거해줬습니다.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl jipsayo | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl jipsayo | grep java | awk '{print $1}')
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
@@ -25,11 +25,8 @@ echo "> JAR Name: $JAR_NAME"
 cd $REPOSITORY
 
 echo "> $JAR_NAME 에 실행권한 추가"
-sudo chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 # 환경변수를 추가가 아닌 대체하는것!
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-rds.yml \
-    -Dspring.profiles.active=dev \
-    $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-rds.yml -Dspring.profiles.active=dev $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
