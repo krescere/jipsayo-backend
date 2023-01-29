26c0040328ca60bbe79352fe651b984edc099719
shininghyunho <gusgh3315@uos.ac.kr>
2023-01-30 02:03:33 +0900

    feat : 이미지 url  리스트 반환 기능을 추가했습니다.

---
+++ src/main/kotlin/krescere/jipsayobackend/service/S3Handler.kt
@@ -0,0 +1,25 @@
+package krescere.jipsayobackend.service
+
+import com.amazonaws.services.s3.AmazonS3Client
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class S3Handler(
+    private val s3Client: AmazonS3Client,
+) {
+    @Value("\${cloud.aws.s3.bucket}")
+    var bucket : String? = null
+
+    @Transactional(readOnly = true)
+    fun getObjects() : List<String> {
+        return s3Client.listObjects(bucket)
+            .objectSummaries.map { it.key }
+    }
+
+    @Transactional(readOnly = true)
+    fun getObjectUrl(key: String) : String {
+        return s3Client.getUrl(bucket, key).toString()
+    }
+}
