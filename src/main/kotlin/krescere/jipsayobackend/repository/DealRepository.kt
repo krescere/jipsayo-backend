532a27e63f91451ee839f84cd7d8ca9f00074984
shininghyunho <gusgh3315@uos.ac.kr>
2023-03-27 16:06:55 +0900

    refact : stream 쿼리를 변경했습니다.

--- src/main/kotlin/krescere/jipsayobackend/repository/DealRepository.kt
+++ src/main/kotlin/krescere/jipsayobackend/repository/DealRepository.kt
@@ -17,6 +17,14 @@ interface DealRepository : JpaRepository<Deal, Long> {
             "from Deal d " +
             "join fetch d.houseDetail hd " +
             "join fetch hd.house h " +
-            "where d.cost < :highCost and d.cost > :lowCost")
+            "where d.cost < :highCost and d.cost > :lowCost " +
+            "and d.dealDate = " +
+            "(select max(d2.dealDate) " +
+            "from Deal d2 " +
+            "join d2.houseDetail hd2 " +
+            "join hd2.house h2 " +
+            "where h2.roadAddress = h.roadAddress " +
+            "and h2.danjiName = h.danjiName) " +
+            "group by h.roadAddress, h.danjiName ")
     fun findDealsByCostRange(@Param("highCost") highCost: Long, @Param("lowCost") lowCost: Long): Stream<DealFilterResponse>
 }
