6e52c97aaeb81bff4baae214e42b2353367c606d
shininghyunho <gusgh3315@uos.ac.kr>
2023-04-06 17:21:18 +0900

    chore : 배포 구성을 변경하였습니다.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,8 +28,6 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-# 환경변수를 추가가 아닌 대체하는것!
 nohup java -jar $JAR_NAME \
-    -Dspring.config.location=classpath:/application.yml,classpath:/application-dev.yml,classpath:/application-rds.yml \
-    -Dspring.profiles.active=dev \
+    -Dspring.config.location=/home/ec2-user/app/application-rds.yml,/home/ec2-user/app/application-s3.yml \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
