fc4da1fc69814e4733f4ab2aa0f1cad45854dd46
shininghyunho <gusgh3315@uos.ac.kr>
2023-04-07 17:12:46 +0900

    chore : s3의 설정을 yaml에 맞게 설정해줬습니다.

--- src/main/kotlin/krescere/jipsayobackend/config/S3Config.kt
+++ src/main/kotlin/krescere/jipsayobackend/config/S3Config.kt
@@ -4,21 +4,23 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import krescere.jipsayobackend.common.YamlPropertySourceFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
 
 @Configuration
-@PropertySource("classpath:application-s3.yml")
-class S3Config {
+@PropertySource(value = ["classpath:application-s3.yml"], factory = YamlPropertySourceFactory::class)
+class S3Config(
     @Value("\${cloud.aws.credentials.access-key}")
-    var accessKey : String? = null
+    val accessKey : String,
     @Value("\${cloud.aws.credentials.secret-key}")
-    var secretKey : String? = null
+    val secretKey : String,
     @Value("\${cloud.aws.region.static}")
-    var region : String? = null
-
+    val region : String
+) {
     @Bean
     fun s3Client() : AmazonS3Client {
         return AmazonS3ClientBuilder.standard()
