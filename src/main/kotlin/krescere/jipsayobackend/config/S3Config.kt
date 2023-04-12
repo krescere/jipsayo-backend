6a58f507738d9891cabd1b567ee64da7d57b4cfc
shininghyunho <gusgh3315@uos.ac.kr>
2023-04-12 19:05:06 +0900

    fixed : s3 profile 호출을 변경했습니다.

--- src/main/kotlin/krescere/jipsayobackend/config/S3Config.kt
+++ src/main/kotlin/krescere/jipsayobackend/config/S3Config.kt
@@ -4,50 +4,29 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import org.springframework.context.annotation.PropertySource
-import org.springframework.core.env.Environment
-import org.springframework.core.io.ClassPathResource
-import org.springframework.core.io.FileSystemResource
+import org.springframework.context.annotation.*
 import java.util.*
 
 @Configuration
+@PropertySources(
+    PropertySource("classpath:application-s3.properties", ignoreResourceNotFound = true),
+    PropertySource("file:/home/ec2-user/app/application-s3.properties", ignoreResourceNotFound = true)
+)
 class S3Config {
+    @Value("\${cloud.aws.credentials.access-key}")
     lateinit var accessKey: String
+    @Value("\${cloud.aws.credentials.secret-key}")
     lateinit var secretKey: String
+    @Value("\${cloud.aws.region.static}")
     lateinit var region: String
-
-    @Autowired
-    lateinit var environment: Environment
-
-    @Profile("local", "dev")
-    private fun setProperties() {
-        val properties = Properties()
-        val resource = if (isActiveProfile("local")) {
-            ClassPathResource("application-s3.properties")
-        } else {
-            FileSystemResource("/home/ec2-user/app/application-s3.properties")
-        }
-        properties.load(resource.inputStream)
-
-        accessKey=properties.getProperty("cloud.aws.credentials.access-key")
-        secretKey=properties.getProperty("cloud.aws.credentials.secret-key")
-        region=properties.getProperty("cloud.aws.region.static")
-    }
 
     @Bean
     fun s3Client(): AmazonS3Client {
-        setProperties()
         return AmazonS3ClientBuilder.standard()
             .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
             .withRegion(region)
             .build() as AmazonS3Client
     }
-    private fun isActiveProfile(profile: String): Boolean {
-        return Arrays.stream(environment.activeProfiles).anyMatch { p -> p == profile }
-    }
+
 }
