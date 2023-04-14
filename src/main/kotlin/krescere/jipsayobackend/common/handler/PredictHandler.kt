403a1e076d147055645ea92c36f297fd42ca8108
shininghyunho <gusgh3315@uos.ac.kr>
2023-04-14 22:47:50 +0900

    fixed : 필터링 후보를 받아오는 부분에 예외 처리를 추가했습니다.

--- src/main/kotlin/krescere/jipsayobackend/common/handler/PredictHandler.kt
+++ src/main/kotlin/krescere/jipsayobackend/common/handler/PredictHandler.kt
@@ -3,6 +3,7 @@ package krescere.jipsayobackend.common.handler
 import com.google.gson.Gson
 import krescere.jipsayobackend.dto.dealHistory.DealHistoryFilterRequest
 import krescere.jipsayobackend.dto.house.HousePredictResponse
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,10 +11,16 @@ class PredictHandler(
     private val httpHandler: ApacheHttpHandler,
     private val gson: Gson
 ) {
+    val logger = LoggerFactory.getLogger(this.javaClass)!!
     fun getCandidateMap(request: DealHistoryFilterRequest) : Map<Long, HousePredictResponse> {
-        val candidateJson = getCandidateJson(request)
-        val candidateList = gson.fromJson(candidateJson, Array<HousePredictResponse>::class.java).toList()
-        return candidateList.associateBy { it.id }
+        return try {
+            val candidateJson = getCandidateJson(request)
+            val candidateList = gson.fromJson(candidateJson, Array<HousePredictResponse>::class.java).toList()
+            candidateList.associateBy { it.id }
+        } catch (e: Exception) {
+            logger.error("getCandidateMap error", e)
+            emptyMap()
+        }
     }
     private fun getCandidateJson(request: DealHistoryFilterRequest) : String {
         val urlBuilder = StringBuilder()
